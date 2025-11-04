a766ca6b5aabd6edc0ebbf4f08a2c159503531cd
Ethan Bostick <ethan.bostick@gmail.com>
2025-11-03 20:42:15 -0500

    new stuff

--- ebostick/main.py
+++ ebostick/main.py
@@ -1,20 +1,36 @@
 import prep as p
+import re
 import sys
 from gensim.models import KeyedVectors
 
 useGpu = False
 
 try:
-    corporaPathes = sys.argv[1:5]
-    try:
-        g = sys.argv[5]
-        useGpu = True
-        print("utilizing GPU")
-    except(Exception):
-        print("utilizing strictly CPUs")
+	ePath = sys.argv[1]
+	gPath = sys.argv[2]
+	dPath = sys.argv[3]
+	lPath = sys.argv[4]
+	try:
+		g = sys.argv[5]
+		useGpu = True
+		print("utilizing GPU")
+	except(Exception):
+		print("utilizing strictly CPUs")
 
 except(Exception):
-    print("see run use:\npython main.py [path_to_corp1] [path_to_corp2] [path_to_corp3] [path_to_corp4] optional:[gpu|cpu]")
+	print("see run use:\npython main.py [path_to_ethanCorp] [path_to_garrettCorp] [path_to_delaneyCorp] [path_to_laraCorp] optional:[gpu|cpu]")
 
-#embeddings
-embeds = KeyedVectors.load("glove_embeddings.data")
+#get embeddings
+try:
+	embeds = KeyedVectors.load("glove_embeddings.data")
+except(Exception):
+	print("no saved embeddings")
+
+#attempt prep corpora
+ethanDelim = [r"\.", r"\?", r"!"]
+garrDelim = [r"<EOD>"]
+delaneyDelim = [r"\n"]
+
+corpora = {}
+corpora[0] = p.createDocs(ePath,ethanDelim) 	
+	
--- ebostick/prep.py
+++ ebostick/prep.py
@@ -1,10 +1,28 @@
 import re
+import string
 
 def createDocs(pathToFile,delimiters):
-#delimiters = [r"\.\.\.", r"\.", r"\?", r"!", r"\n", r"<EOD>"]
+	#delimiters = [r"\.\.\.", r"\.", r"\?", r"!", r"\n", r"<EOD>"]
     with open(pathToFile,"r",encoding="utf-8") as f:
         text = f.read()
+	#split to get documents by given delimiters
+	print("fetching documents")
     pattern = r"(?:{})(?:\s+|$)".format("|".join(delimiters)) 
     sentences = [s.strip() for s in re.split(pattern, text) if s.strip()]
+
+	#clean and filter
+	i = 0
+	translator = str.maketrans('', '', string.punctuation)
+	for sentence in sentences:
+		#drop punc
+		sentences[i] = text.translate(translator)
+		#drop if empty
+		if not sentence:
+			del sentence[i]		
+
+		if i%10 == 0:
+			print("cleaning documents")
+		i+=1
+	return sentences
 
     
